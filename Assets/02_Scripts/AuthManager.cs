3ee1c9e20fd78c58f447f7dbc15d6c417d196960
cookie3029 <whitewalls@naver.com>
2024-09-24 11:06:55 +0900

    UI 연결

---
+++ Assets/02_Scripts/AuthManager.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    [SerializeField] private Button signInButton;
+    [SerializeField] private TMP_Text messageText;
+}
