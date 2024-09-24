5721288082b1143e10c07576bddc0b1764b0939b
cookie3029 <whitewalls@naver.com>
2024-09-24 11:32:33 +0900

    익명 로그인 로직 구현

--- Assets/02_Scripts/AuthManager.cs
+++ Assets/02_Scripts/AuthManager.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,4 +9,44 @@ public class AuthManager : MonoBehaviour
 {
     [SerializeField] private Button signInButton;
     [SerializeField] private TMP_Text messageText;
+
+    private async void Awake()
+    {
+        // USG 초기화
+        await UnityServices.InitializeAsync();
+
+        // 버튼 이벤트 연결
+        signInButton.onClick.AddListener(async () =>
+        {
+            // 익명 로그인
+            await SignInAsync();
+        });
+    }
+
+    // 익명 로그인 처리
+    private async Task SignInAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("익명 로그인 성공");
+        }
+        catch (AuthenticationException e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+
+    /*
+        C# Thread Programming
+
+        Async / Await
+
+        async void/Task 함수명()
+        {
+            로직1
+            await 작업
+            로직2
+        }
+    */
 }
